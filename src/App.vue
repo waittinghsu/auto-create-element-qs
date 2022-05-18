bc7da96a3a67376fa2c107419ce0f126dacb15f8
spidergod <spidergod23@gmail.com>
2022-05-19 02:43:06 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <div
+      style="
+        display: flex;
+        flex-direction: column;
+        justify-content: cneter;
+        align-items: center;
+      "
+    >
+      <template v-for="item in columns">
+        <div
+          :key="item.key"
+          style="display: flex; flex-direction: row; padding: 8px 0"
+        >
+          <label :for="item.key" style="padding-right: 10px">{{
+            item.key
+          }}</label>
+          <HelloWorld v-model="form" :type="item.type" :path="item.path" />
+        </div>
+      </template>
+    </div>
+    {{ form }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      form: {
+        a: {
+          b: {
+            c: 1,
+          },
+        },
+        d: "1231",
+      },
+      columns: [
+        {
+          key: "accounnt",
+          path: "a.b.c",
+          type: "input",
+        },
+        {
+          key: "commend",
+          path: "d",
+          type: "number",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hellos">
+    <input :type="type" @input="handleInput" />
+  </div>
+</template>
+
+<script>
+import { cloneDeep, set } from "lodash-es";
+export default {
+  name: "HelloWorld",
+  model: {
+    prop: "modelValue",
+    event: "update",
+  },
+  props: {
+    modelValue: {
+      type: Object,
+      default: () => {},
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    path: {
+      type: String,
+      default: "text",
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleInput(event) {
+      console.log(Object.prototype.toString.call(event), event.target.value);
+      const modelValue = cloneDeep(this.modelValue);
+      set(modelValue, this.path, event.target.value);
+      this.$emit("update", modelValue);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
